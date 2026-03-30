c4e2999e64d378e4fb281b2705dc892f78a58ead
cf-yharbar <yaroslav.harbar@cloudfide.com>
2026-03-30 12:34:50 +0200

    feat: producer analytics

---
+++ src/main/java/com/example/cloudfide_igor_barcikowski_task/analytics/EmailService.java
@@ -0,0 +1,10 @@
+package com.example.cloudfide_igor_barcikowski_task.analytics;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    public void sendReactivationEmail(Long producerId, String producerName) {
+    }
+}
